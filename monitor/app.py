2f350c3f3851f1e26c3284059eb877990a23b155
charlotte marinière <88865416+charlottemariniere@users.noreply.github.com>
2021-12-01 21:40:37 +0800

    Environmental data monitoring and storage

--- monitor/app.py
+++ monitor/app.py
@@ -5,6 +5,12 @@ from flask_cors import CORS
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 
+
+import random
+# import logging
+# import current as current
+# import self as self
+
 db_filename = './data/monitor_data.db'
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -17,11 +23,11 @@ Base.metadata.create_all(engine)
 
 def set_status(status):
     if status is True:
-        return 'Running', 'fa fa-smile', 'bg-success'
+        return 'All good', 'fa fa-smile', 'bg-success'
     elif status is False:
-        return 'Stopped', 'fas fa-angry', 'bg-danger'
+        return 'Not working', 'fas fa-angry', 'bg-danger'
     else:
-        return 'Unknown', 'fas fa-sad-tear', 'bg-dark'
+        return 'Unknown error', 'fas fa-sad-tear', 'bg-dark'
 
 
 @app.route('/')
@@ -52,6 +58,9 @@ def index():
     return render_template('index.html', statuses=statuses)
 
 
+
+
+
 @app.route('/about')
 def demo_template():
     return render_template('about.html')
@@ -65,15 +74,10 @@ def cpu_load(qty=1):
         qty = 1
     active_session = session()
     cpu = active_session.query(CPU).order_by(CPU.created_at.desc()).first()
-    data = {cpu.id: {
+    data = {cpu.id:{
+        'host_name':cpu.host_name,
         'created_at': cpu.created_at,
-        'host_mac': cpu.host_mac,
-        'cpu_temp': cpu.cpu_temp,
-        'gpu_temp': cpu.gpu_temp,
-        'host_name': cpu.host_name,
-        'id': cpu.id,
-        'load': cpu.load,
-        'serial': cpu.serial,
+        'load':cpu.load
     }}
     return jsonify(data)
 
@@ -83,21 +87,22 @@ def cpu_load_latest():
     return cpu_load(1)
 
 
-# @app.route('api/device-load')
-# def device_load():
-#     return device_load(1)
+@app.route('/api/device-load')
+def return_current_cpu():
+    data = current_cpu()
+    return {"CPU": data}
 
 
 @app.route('/api/environment')
 def get_api_environment():
-    current_enviro = get_api_temperature(), get_api_pressure(), get_api_humidity()
-    return {"Environment": current_enviro}
+    current_environment = get_api_temperature(), get_api_pressure(), get_api_humidity()
+    return {"Environment": current_environment}
 
 
 @app.route('/api/temperature')
 def get_api_temperature():
-    current_temp = EnvironmentTPH().temperature
-    return {"Temperature": current_temp}
+    current_temperature = EnvironmentTPH().temperature
+    return {"Temperature": current_temperature}
 
 
 @app.route('/api/pressure')
@@ -109,20 +114,20 @@ def get_api_pressure():
 @app.route('/api/humidity')
 def get_api_humidity():
     current_humidity = EnvironmentTPH().humidity
-    return {"Pressure": current_humidity}
+    return {"Humidity": current_humidity}
 
 
-@app.route('/api/history/')
+@app.route('/api/history')
 def temp_history():
     @app.route('/api/ticker')
-    def something():
+    def random_number_ticker():
         x = 0
         for num in range(1):
-            r = random.randint(0, 50)
-            x = r
+            random_number = random.randint(0, 100)
+            x = random_number
             return {"History": x}
-        return render_template("Historical_page.html")
+    return render_template("history.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5555)
